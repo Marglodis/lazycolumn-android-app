7a5795ac3ece5115b3cb12fb894d0b3df0b9b745
Marglodis Tovar <marglodistovar@gmail.com>
2025-10-01 16:12:42 -0300

    Agregar agregar el padding del statusbar

--- app/src/main/java/com/mtovar/lazycolumnapp/ui/theme/screens/TreeListScreen.kt
+++ app/src/main/java/com/mtovar/lazycolumnapp/ui/theme/screens/TreeListScreen.kt
@@ -1,6 +1,8 @@
 package com.mtovar.lazycolumnapp.ui.theme.screens
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -11,7 +13,9 @@ import com.mtovar.lazycolumnapp.data.ChileanTreeData
 @Composable
 fun TreeListScreen (modifier: Modifier){
 
-    Column {
+    Column(
+        modifier = Modifier.statusBarsPadding().fillMaxSize()
+    ) {
         LazyColumn {
             items(ChileanTreeData.chileanTreeList) { tree ->
                 TreeItem(modifier, tree)
